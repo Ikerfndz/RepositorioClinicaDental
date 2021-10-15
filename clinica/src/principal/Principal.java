26b0e6808a8603cc6536fe6516e0e69d801d11d6
alumnado <alumnado@A204PC22.desarrolloweb.local>
2021-10-15 17:27:36 +0200

    COMIT DE IKER

--- clinica/src/principal/Principal.java
+++ clinica/src/principal/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-		System.out.println("Hola!");
+		System.out.println("HolaVHKKTCFT!");
 
 	}
 
