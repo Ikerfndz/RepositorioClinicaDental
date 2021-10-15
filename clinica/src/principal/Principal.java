fe1f1ced6391d92e0f693ad0853288654faa7e3b
alumnado <alumnado@a204pc21.desarrolloweb.local>
2021-10-15 17:21:00 +0200

    primer commit

---
+++ clinica/src/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		System.out.println("Hola!");
+
+	}
+
+}
