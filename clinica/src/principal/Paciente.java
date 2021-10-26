80f639c728efb76c47a123645e022b4dff31e8c1
usut25 <usut25@A204PC23.desarrolloweb.local>
2021-10-26 17:09:40 +0200

    Inicio

--- clinica/src/principal/Paciente.java
+++ clinica/src/principal/Paciente.java
@@ -6,4 +6,25 @@ public class Paciente {
 		// TODO Auto-generated constructor stub
 	}
 
+	protected String nombre;
+	protected int id;
+	protected int edad;
+	protected Historial historial = new Historial();
+	
+
+	public Paciente(String nombre, int id) {
+		super();
+		this.nombre = nombre;
+		this.id = id;
+	}
+
+
+	public Paciente(String nombre, int id, int edad, Historial documentacion, Historial historial) {
+		super();
+		this.nombre = nombre;
+		this.id = id;
+		this.edad = edad;
+		this.historial = historial;
+	}
+
 }
