68d089ce10bd6ad68e26ce3eef77ea9cc742cf89
usut15 <usut15@A204PC19.desarrolloweb.local>
2021-11-04 18:02:27 +0100

    comit de miguel

--- clinica/src/principal/Empleado.java
+++ clinica/src/principal/Empleado.java
@@ -1,5 +1,22 @@
 package principal;
 
 public class Empleado {
+	
+	
+	
+	protected long id;
+	protected String nombre;
+	protected String apellidos;
+	protected long telefono;
+	protected String direccion;
+	protected long nif;
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
