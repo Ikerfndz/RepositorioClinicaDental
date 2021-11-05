ca1bfee43da00b050521d9f998dda7be59134aaa
raul_ <raul_@DESKTOP-MHK8MRK>
2021-11-05 23:49:29 +0100

    Segundo comit Raul

--- clinica/src/principal/Cirujano.java
+++ clinica/src/principal/Cirujano.java
@@ -1,17 +1,40 @@
 package principal;
 
-public class Cirujano extends Empleado {
 
-	private Especialidad especialidad = new Especialidad();
+public class Cirujano extends Empleado {
+	private Especialidad especialidades[]=new Especialidad[10];
+	private Intervencion intervenciones[]=new Intervencion[10];
+	
+
 
 	public Cirujano() {
 	}
-
-	public Especialidad getEspecialidad() {
-		return especialidad;
+	
+	public Cirujano(Especialidad especialidad,Empleado empleado,Intervencion intervencion) {
+		super(empleado);
+		especialidades[0]=especialidad;
+		intervenciones[0]=intervencion;
+				
+	}
+	
+	public static Cirujano nuevoCirujano() {
+		Empleado empleado=Empleado.nuevoEmpleado();
+		Intervencion intervencion=Intervencion.nuevaIntervencion();
+		Especialidad especialidad=Especialidad.nuevaEspecialidad();
+		Intervencion intervenciones=Intervencion.nuevaIntervencion();
+		Cirujano aux=new Cirujano(especialidad,empleado,intervencion);
+		especialidad.setCirujano(aux);
+		return aux;
 	}
 
-	public void setEspecialidad(Especialidad especialidad) {
-		this.especialidad = especialidad;
+	public Especialidad[] getEspecialidades() {
+		return especialidades;
 	}
+
+
+	public void setEspecialidades(Especialidad[] especialidades) {
+		this.especialidades = especialidades;
+	}
+	
+
 }
--- clinica/src/principal/Enfermeria.java
+++ clinica/src/principal/Enfermeria.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 
 public class Enfermeria extends Empleado {
 	private char categoria;
+	private Intervencion intervenciones[]=new Intervencion[10];
+	
 
 
 	public Enfermeria() {
 
 	}
-
+	public Enfermeria(char categoria,Empleado empleado,Intervencion intervencion) {
+		super(empleado);
+		setCategoria(categoria);
+		intervenciones[0]=intervencion;
+	}
 
 	public static Enfermeria nuevoEnfermero() {
-		Enfermeria ret = new Enfermeria();
+		Empleado empleado=Empleado.nuevoEmpleado();
+		Intervencion intervencion=Intervencion.nuevaIntervencion();
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Introduce la categoría del enfermero/a");
 		char cat;
 		cat=teclado.next().charAt(0);
-		ret.setCategoria(cat);
+		Enfermeria ret= new Enfermeria(cat,empleado,intervencion);
 		teclado.close();
 		return ret;
 	}
