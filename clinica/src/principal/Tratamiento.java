6e12281918c7be9275d06583dba2eaa862578979
Iker <Iker@DESKTOP-6T3PVDV>
2021-11-06 15:50:07 +0100

    Tratamientos

--- clinica/src/principal/Tratamiento.java
+++ clinica/src/principal/Tratamiento.java
@@ -1,5 +1,56 @@
 package principal;
 
-public class Tratamiento {
+import java.util.Scanner;
 
+public class Tratamiento {
+	
+	protected long idTratamiento;
+	protected String nombreDescriptivo;
+	protected boolean consentimiento= true;
+	
+	
+	public static Tratamiento nuevoTratammiento() {
+		Tratamiento ret = new Tratamiento();
+		Scanner teclado;
+		teclado = new Scanner(System.in);
+		
+		System.out.println("Introduzca el nombre del nuevo tratamiento");
+		String nom="";
+		nom= teclado.nextLine();
+		ret.setNombreDescriptivo(nom);
+		return ret;
+	}
+	
+	
+	
+	
+	
+	
+	public long getIdTratamiento() {
+		return idTratamiento;
+	}
+	public void setIdTratamiento(long idTratamiento) {
+		this.idTratamiento = idTratamiento;
+	}
+	public String getNombreDescriptivo() {
+		return nombreDescriptivo;
+	}
+	public void setNombreDescriptivo(String nombreDescriptivo) {
+		this.nombreDescriptivo = nombreDescriptivo;
+	}
+	public boolean isConsentimiento() {
+		return consentimiento;
+	}
+	public void setConsentimiento(boolean consentimiento) {
+		this.consentimiento = consentimiento;
+	}
+	
+	@Override
+	public String toString() {
+		return "Tratamiento [idTratamiento=" + idTratamiento + ", nombreDescriptivo=" + nombreDescriptivo
+				+ ", consentimiento=" + consentimiento + "]";
+	}
+	
+	
+	
 }
