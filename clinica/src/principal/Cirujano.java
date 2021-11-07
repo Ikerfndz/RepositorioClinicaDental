9b5a11a1a12d0e62affa76f9d9f542143100e516
raul_ <raul_@DESKTOP-MHK8MRK>
2021-11-07 23:54:46 +0100

    Cuarto commit Raul Cirujano Enfermeria Especialidad

--- clinica/src/principal/Cirujano.java
+++ clinica/src/principal/Cirujano.java
@@ -1,49 +1,66 @@
 package principal;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Cirujano extends Empleado {
-	
-	private Especialidad especialidades[]=new Especialidad[10]; //Array donde se guarda el numero de especialidades que puede tener un cirujano.
-	private Intervencion intervenciones[]=new Intervencion[10];	//Array donde se guarda el numero de intervenciones en las que interviene un cirujano.
-	
-	
-	//Constructor por defecto
+
+	private boolean tieneEsp = false;
+	// Atributo de tipo boolean para declarar si un cirujano tiene o no
+	// especialidad.
+	private Especialidad especialidades[] = new Especialidad[10];
+	// Array donde se guarda el numero de especialidades que puede tener un
+	// cirujano.
+	private Intervencion intervenciones[] = new Intervencion[10];
+	// Array donde se guarda el numero de intervenciones en las que interviene un
+	// cirujano.
+
+	// Constructor por defecto
 	public Cirujano() {
 	}
-	
-	//Constructor que recibe por parametros la especialidad el empleado y la intervención.
-	//Primero hace una llamada a super para sacar los datos del empleado.
-	//Se guarda la especialidad en el array.
-	//Se guarda la intervencion en el array.
-	
-	public Cirujano(Especialidad especialidad,Empleado empleado,Intervencion intervencion) {
+
+	// Constructor que recibe por parametros la especialidad el empleado y la
+	// intervención.
+	// Primero hace una llamada a super para sacar los datos del empleado.
+	// Se guarda la especialidad en el array.
+	// Se guarda la intervencion en el array.
+
+	public Cirujano(Especialidad especialidad, Empleado empleado, Intervencion intervencion, boolean tieneEsp) {
 		super(empleado);
-		especialidades[0]=especialidad;
-		intervenciones[0]=intervencion;
-		
+		especialidades[0] = especialidad;
+		intervenciones[0] = intervencion;
+		this.tieneEsp = tieneEsp;
+
 	}
-	
-	//Método encargado de crear un nuevo cirujano.
-	//Se declara una variable de tipo Empleado donde se guardan los datos del empleado haciendo llamada al método nuevoEmpleado.
-	//Se declara una variable de tipo Intervencion donde se guardan los datos de la intervención haciendo llamada al método nuevaIntervencion.
-	//Se declara una variable de tipo Especialidad donde se guardan los datos de la especialidad haciendo llamada al método nuevaEspecialidad.
-	//Se declara una variable de tipo Cirujano que se instancia haciendo llamada al constructor anterior y se le pasa por parametros las variables creadas en este metodo.
-	//Se guarda el cirujano creado en la clase Especialidad porque cada Especialidad pertenece a un cirujano.
+
+	// Método encargado de crear un nuevo cirujano.
+	// Se declara una variable de tipo Empleado donde se guardan los datos del
+	// empleado haciendo llamada al método nuevoEmpleado.
+	// Se declara una variable de tipo Intervencion donde se guardan los datos de la
+	// intervención haciendo llamada al método nuevaIntervencion.
+	// Se declara una variable de tipo Especialidad donde se guardan los datos de la
+	// especialidad haciendo llamada al método nuevaEspecialidad.
+	// Se declara una variable de tipo Cirujano que se instancia haciendo llamada al
+	// constructor anterior y se le pasa por parametros las variables creadas en
+	// este metodo.
+	// Se guarda el cirujano creado en la clase Especialidad porque cada
+	// Especialidad pertenece a un cirujano.
 	public static Cirujano nuevoCirujano() {
-		Empleado empleado=Empleado.nuevoEmpleado();
-		Intervencion intervencion=Intervencion.nuevaIntervencion();
-		Especialidad especialidad=Especialidad.nuevaEspecialidad();
-		Cirujano ret=new Cirujano(especialidad,empleado,intervencion);
+		Empleado empleado = Empleado.nuevoEmpleado();
+		Intervencion intervencion = Intervencion.nuevaIntervencion();
+		Especialidad especialidad = Especialidad.nuevaEspecialidad();
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("¿Tiene el cirujano especialidad?");
+		boolean aux = teclado.nextBoolean();
+		Cirujano ret = new Cirujano(especialidad, empleado, intervencion, aux);
 		especialidad.setCirujano(ret);
 		return ret;
 	}
-	
-	//Getters y setters
+
+	// Getters y setters
 	public Especialidad[] getEspecialidades() {
 		return especialidades;
 	}
-
 
 	public void setEspecialidades(Especialidad[] especialidades) {
 		this.especialidades = especialidades;
@@ -57,12 +74,11 @@ public class Cirujano extends Empleado {
 		this.intervenciones = intervenciones;
 	}
 
-	//To string
+	// To string
 	@Override
 	public String toString() {
 		return "Cirujano [especialidades=" + Arrays.toString(especialidades) + ", intervenciones="
 				+ Arrays.toString(intervenciones) + "]";
 	}
-	
 
 }
--- clinica/src/principal/Enfermeria.java
+++ clinica/src/principal/Enfermeria.java
@@ -4,46 +4,53 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Enfermeria extends Empleado {
-	private char categoria; // Categoría de el/la enfemero/a codificada con una letra
-	private Intervencion intervenciones[]=new Intervencion[10]; // Array donde se guarda el numero de intervenciones que puede tener un/a enfermero/a.
+	private char categoria;
+	// Categoría de el/la enfemero/a codificada con una letra
+	private Intervencion intervenciones[] = new Intervencion[10];
+	// Array donde se guarda el numero de intervenciones que puede tener un/a
+	// enfermero/a.
 
-
-
-	public Enfermeria() { 
-		//Contructor por defecto
+	public Enfermeria() {
+		// Contructor por defecto
 	}
-	
-	// Constructor que recibe como parámetros atributos de tipo char, Empleado, e Intervención
+
+	// Constructor que recibe como parámetros atributos de tipo char, Empleado, e
+	// Intervención
 	// Hace una llamada a su clase super para sacar sus atributos
-	// Guarda la categoría con el set ( Lo hago llamado al set pero también podría ser con this.categoria=categoria)
-	//Guarda la intervención en el array intervenciones.
-	
-	public Enfermeria(char categoria,Empleado empleado,Intervencion intervencion) { 
+	// Guarda la categoría con el set ( Lo hago llamado al set pero también podría
+	// ser con this.categoria=categoria)
+	// Guarda la intervención en el array intervenciones.
+
+	public Enfermeria(char categoria, Empleado empleado, Intervencion intervencion) {
 		super(empleado);
 		setCategoria(categoria);
-		intervenciones[0]=intervencion;
+		intervenciones[0] = intervencion;
 	}
 
-	
-	//Método nuevoEnfermero encargado de registrar a un nuevo enfermero.
-	//Se declara una variable de tipo Empleado donde se guarda la información del empleado haciendo llamada al método nuevoEmpleado.
-	//Se declara una variable de tipo Intervencion donde se guarda la información de la intervencion haciendo llamada al método nuevaIntervencion.
-	//Se pide por pantalla que introduzca la categoría del enfermero con el teclado y se guarda en una variable auxiliar llamada cat.
-	//Finalmente se declara y se inicializa una variable de tipo Enfermería (llamando al constructor anterior) que será la que utilizaremos para devolver todos los datos introducidos en este metodo .
+	// Método nuevoEnfermero encargado de registrar a un nuevo enfermero.
+	// Se declara una variable de tipo Empleado donde se guarda la información del
+	// empleado haciendo llamada al método nuevoEmpleado.
+	// Se declara una variable de tipo Intervencion donde se guarda la información
+	// de la intervencion haciendo llamada al método nuevaIntervencion.
+	// Se pide por pantalla que introduzca la categoría del enfermero con el teclado
+	// y se guarda en una variable auxiliar llamada cat.
+	// Finalmente se declara y se inicializa una variable de tipo Enfermería
+	// (llamando al constructor anterior) que será la que utilizaremos para devolver
+	// todos los datos introducidos en este metodo .
 	public static Enfermeria nuevoEnfermero() {
-		Empleado empleado=Empleado.nuevoEmpleado();
-		Intervencion intervencion=Intervencion.nuevaIntervencion();
+		Empleado empleado = Empleado.nuevoEmpleado();
+		Intervencion intervencion = Intervencion.nuevaIntervencion();
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Introduce la categoría del enfermero/a");
 		char cat;
-		cat=teclado.next().charAt(0);
-		Enfermeria ret= new Enfermeria(cat,empleado,intervencion);
+		cat = teclado.next().charAt(0);
+		Enfermeria ret = new Enfermeria(cat, empleado, intervencion);
 		teclado.close();
 		return ret;
 	}
 
-	//Getters y Setters de los atributos 
-	
+	// Getters y Setters de los atributos
+
 	public char getCategoria() {
 		return categoria;
 	}
@@ -51,9 +58,11 @@ public class Enfermeria extends Empleado {
 	public void setCategoria(char categoria) {
 		this.categoria = categoria;
 	}
+
 	public Intervencion[] getIntervenciones() {
 		return intervenciones;
 	}
+
 	public void setIntervenciones(Intervencion[] intervenciones) {
 		this.intervenciones = intervenciones;
 	}
@@ -62,7 +71,5 @@ public class Enfermeria extends Empleado {
 	public String toString() {
 		return "Enfermeria [categoria=" + categoria + ", intervenciones=" + Arrays.toString(intervenciones) + "]";
 	}
-
-
 
 }
