899d9a3c94b20eff9e0b6d2bdb19ef36f9973801
usut07 <usut07@A204PC22.desarrolloweb.local>
2021-11-10 18:38:49 +0100

    bf

--- clinica/src/principal/Paciente.java
+++ clinica/src/principal/Paciente.java
@@ -8,27 +8,26 @@ public class Paciente {
 	public Paciente() {
 
 	}
-
-	private String nombre;
 	// nombre representa el nombre del paciente.
 	// es una cadena de caracteres con un minimo de 3 caracteres y maximo de 50.
 	// no acepta numeros ni caracteres especiales .
-	private long idPaciente;
+	private String nombre;
 	// idPaciente representa al identificador único del paciente.
 	// es un valos entero >0 .
-	private String telefono;
+	private long idPaciente;
 	// telefono representa al numero de telfono del paciente.
-
-	private String direccion;
+	// esta formado por 9 digitos
+	private String telefono;
 	// direccion representa la direccion del paciente.
 	// esta formado por una cadena de caracteres de minimo 5 caracteres y un maximo
 	// de 50
-	private String edad;
+	private String direccion;
 	// edad representa a la edad del paciente
-	
-	private String nif;
+	private String edad;
 	// nif representa nif del paciente
 	// esta formado por 8 digitos y 1 caracter
+	private String nif;
+	
 	private Historial historial = new Historial();
 	private Tratamiento tratamientos[] = new Tratamiento[10];
 
