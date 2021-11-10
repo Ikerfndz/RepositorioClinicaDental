6d15570b7df82c8f53482769d67af992c2b53e9c
usut19 <usut19@a204pc21.desarrolloweb.local>
2021-11-10 19:12:06 +0100

    jkji

--- clinica/src/Validación/Validador.java
+++ clinica/src/Validación/Validador.java
@@ -1,5 +1,17 @@
 package Validación;
 
 public class Validador {
+	
+	public static boolean validarNombreMedicamento(String nombre2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static boolean validarNombreAlergia(String nom) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 
 }
