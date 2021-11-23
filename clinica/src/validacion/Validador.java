3e42a651e161dd0bcb60a6ae88ad493eb780104f
Usuario <Usuario@192.168.1.38>
2021-11-23 13:21:41 +0100

    Correcion

--- clinica/src/validacion/Validador.java
+++ clinica/src/validacion/Validador.java
@@ -24,35 +24,44 @@ public class Validador {
 	// VALIDADORES EMPLEADO
 
 	public static boolean validarNombreEmpleado(String nomEmpleado) {
-		if (nomEmpleado.length() < 2 || nomEmpleado.length() > 30)
+		if (nomEmpleado.length() < 2 || nomEmpleado.length() > 30) {
 			return false;
-		return true;
+		} else {
+			return true;
+		}
 	}
 
 	public static boolean validarApellidoEmpleado(String apeEmpleado) {
-		if (apeEmpleado.length() < 2 || apeEmpleado.length() > 60)
+		if (apeEmpleado.length() < 2 || apeEmpleado.length() > 60) {
 			return false;
-		return true;
+		} else {
+			return true;
+		}
 	}
 
 	public static boolean validarTelefonoValido(String telEmpleado) {
 		if (telEmpleado.length() == 8 && (telEmpleado.charAt(0) == 9 || telEmpleado.charAt(0) == 8
-				|| telEmpleado.charAt(0) == 6 || telEmpleado.charAt(0) == 7))
+				|| telEmpleado.charAt(0) == 6 || telEmpleado.charAt(0) == 7)) {
 			return true;
-		return false;
+		} else {
+			return false;
+		}
 	}
 
 	public static boolean validarDireccionEmpleado(String dirEmpleado) {
-		if (dirEmpleado.length() < 2 || dirEmpleado.length() > 150)
+		if (dirEmpleado.length() < 2 || dirEmpleado.length() > 150) {
 			return false;
-		return true;
-
+		} else {
+			return true;
+		}
 	}
 
 	public static boolean validarnifEmpleado(String niEmpleado) {
-		if (niEmpleado.length() != 8)
+		if (niEmpleado.length() != 8) {
 			return false;
-		return true;
+		} else {
+			return true;
+		}
 	}
 
 }
